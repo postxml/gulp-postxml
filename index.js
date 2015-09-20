20697e0fc92cb7c77a4fedb52bf1410858c3e3c9
Yevgeny Petukhov <petukhov.yevgeny@gmail.com>
2015-09-20 11:11:13 +0300

    Change method to 0.2.x version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var through = require('through2');
 
 module.exports = function (plugins, opts) {
    return through.obj(function (chunk, enc, callback) {
-      var file = postxml(String(chunk.contents), plugins, opts);
+      var file = postxml(plugins).process(String(chunk.contents), opts);
       chunk.contents = new Buffer(file);
       callback(null, chunk);
    });
